c576bf8037ba5e078e334f1d13b845fb3d5f07ac
Rayest <634178463@qq.com>
2016-11-12 21:20:34 +0800

    配置项目的 http 编码问题

---
+++ src/main/java/com/rayeston/httpEncoding/HttpEncodingProperties.java
@@ -0,0 +1,31 @@
+package com.rayeston.httpEncoding;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.nio.charset.Charset;
+
+/**
+ * Created by Rayest on 2016/11/12 0012.
+ */
+@ConfigurationProperties(prefix = "spring.http.encoding")
+public class HttpEncodingProperties {
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    private Charset charset = DEFAULT_CHARSET;
+    private boolean force = true;
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    public void setCharset(Charset charset) {
+        this.charset = charset;
+    }
+
+    public boolean isForce() {
+        return force;
+    }
+
+    public void setForce(boolean force) {
+        this.force = force;
+    }
+}
