f7fff62240626d1540aa9450fb16c08996a369fb
lirui <634178463@qq.com>
2017-09-24 17:17:15 +0800

    主题模式，不同之处仅在于代码中出现的topic和queue

---
+++ src/main/java/com/rayeston/jms/topic/AppProducer.java
@@ -0,0 +1,30 @@
+package com.rayeston.jms.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created by lirui on 2017/9/24.
+ */
+
+public class AppProducer {
+    public static final String url = "tcp://127.0.0.1:61616";
+    public static final String topicName = "topic-test";
+    public static final String textContent = "test";
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createTopic(topicName);
+        MessageProducer messageProducer = session.createProducer(destination);
+        for (int i = 0; i < 100; i++) {
+            TextMessage textMessage = session.createTextMessage(textContent + i);
+            messageProducer.send(textMessage);
+            System.out.println("发送消息" + textMessage.getText());
+        }
+        connection.close();
+    }
+}
