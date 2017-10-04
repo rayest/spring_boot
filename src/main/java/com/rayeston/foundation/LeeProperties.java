0fc70e3abbfc8c1b91be65008423ddefb0cc5f54
lirui <634178463@qq.com>
2017-10-05 01:59:30 +0800

    properties 类: validation

--- src/main/java/com/rayeston/foundation/LeeProperties.java
+++ src/main/java/com/rayeston/foundation/LeeProperties.java
@@ -1,9 +1,14 @@
 package com.rayeston.foundation;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,23 +19,36 @@ import java.util.List;
  */
 @Data
 @ConfigurationProperties("lee")
+@PropertySource("classpath:config/application.properties")
 @Configuration
+@Validated
 public class LeeProperties {
 
+    @NotNull
     private boolean enabled;
 
+    @NotNull
     private InetAddress remoteAddress;
 
-    private final Security security = new Security();
+    @Valid
+    private Security security = new Security();
 
     @Data
     public static class Security {
 
+        @NotEmpty
         private String username;
 
+        @NotEmpty
         private String password;
 
         private List<String> roles = new ArrayList<>(Collections.singleton("USER"));
 
+    }
+
+    public LeeProperties() {
+        this.enabled = enabled;
+        this.remoteAddress = remoteAddress;
+        this.security = security;
     }
 }
