264868c319e6626377bc79377a30408aab30234f
lirui <634178463@qq.com>
2017-10-05 00:18:52 +0800

    通过@value 注入全局值

--- src/main/java/com/rayeston/foundation/MyCommandLineRunner.java
+++ src/main/java/com/rayeston/foundation/MyCommandLineRunner.java
@@ -1,18 +1,25 @@
 package com.rayeston.foundation;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 /**
  * Created by lirui on 2017/10/4.
  */
 @Api("commandLineRunner")
+@Order(1)
 @Configuration
 public class MyCommandLineRunner implements CommandLineRunner {
+
+    @Value("${name}")
+    private String name;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("SpringApplication.run()方法被调用前执行，以初始化一些必要的操作");
+        System.out.println(this.name);
     }
 }
