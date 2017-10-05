3dd727d0fda85c86d489a09b6402424978551df6
lirui <634178463@qq.com>
2017-10-05 16:52:34 +0800

    404异常处理

---
+++ src/main/java/com/rayeston/exception/HttpErrorHandler.java
@@ -0,0 +1,32 @@
+package com.rayeston.exception;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by lirui on 2017/10/5.
+ */
+@RestController
+public class HttpErrorHandler implements ErrorController {
+
+    private final static String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
+    public String errorHtml(HttpServletRequest request) {
+        return "404";
+    }
+
+    @RequestMapping(value = ERROR_PATH)
+    public Object error(HttpServletRequest request) {
+        return "404";
+    }
+
+}
